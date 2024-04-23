d5c2611257cf0515256d45371d2fc82dd281f90c
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-04-23 07:19:33 -0300

    Vetores e Arrays: Adicionar elemento no final do vetor

--- src/vetor/Vetor.java
+++ src/vetor/Vetor.java
@@ -3,8 +3,29 @@ package vetor;
 public class Vetor {
 
     public String[] elementos;
+    public int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    /* public void adiciona(String elemento) {
+        for (int i=0; i<this.elementos.length; i++) {
+            if (this.elementos[i] == null) {
+                elementos[i] = elemento;
+                break;
+            }
+        }
+    } */
+
+    public boolean adiciona(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        System.err.println("Não foi possível adicionar o último elemento, excede a capacidade do vertor.");
+        return false;
     }
 }
--- src/vetor/test/Aula02.java
+++ src/vetor/test/Aula02.java
@@ -4,8 +4,10 @@ import vetor.Vetor;
 
 public class Aula02 {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
-        int i=0;
-        i++;
+        Vetor vetor = new Vetor(2);
+        vetor.adiciona("Java");
+        vetor.adiciona("Angular");
+        vetor.adiciona("Estrutura de Dados");
+        System.out.println();
     }
 }
