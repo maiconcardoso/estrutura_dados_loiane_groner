dc015e362abb31273af64eba866f92b92dd51124
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-04-26 06:48:29 -0300

    Criando uma classe modelo

---
+++ src/model/Contato.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Contato {
+    
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "[Nome: " + this.nome + ", Telefone: " + this.telefone + ", Email: " + this.email + "]\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contato other = (Contato) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
+    
+}
