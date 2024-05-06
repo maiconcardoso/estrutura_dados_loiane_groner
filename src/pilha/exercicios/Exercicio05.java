2e23179c854e914a6bcbd58156ea7dae54723c46
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-05-06 05:54:31 -0300

    Exercicio 05 convertendo decimal em binário

---
+++ src/pilha/exercicios/Exercicio05.java
@@ -0,0 +1,33 @@
+package pilha.exercicios;
+
+import java.util.Stack;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        imprimirResultado(1);
+        imprimirResultado(10);
+        imprimirResultado(2);
+        imprimirResultado(5);
+    }
+
+    public static void imprimirResultado(Integer numero) {
+        System.out.println("O binário do número " + numero + " é: " + converterDecimalBinario(numero));
+    }
+
+    public static String converterDecimalBinario(Integer numero) {
+        Stack<Integer> pilha = new Stack<>();
+        String numBinario = "";
+        int resto;
+        while (numero > 0) {
+            resto = numero % 2;
+            pilha.push(resto);
+            numero = numero / 2;
+        }
+
+        while (!pilha.isEmpty()) {
+            numBinario += pilha.pop();
+            
+        }
+        return numBinario;
+    }
+}
