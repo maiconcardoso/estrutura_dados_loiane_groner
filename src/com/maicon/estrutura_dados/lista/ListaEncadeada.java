28b11b8c0142cb7823639c3c0539ad8f14c3881c
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-05-09 06:11:22 -0300

    Criando o tamanho da lista

--- src/com/maicon/estrutura_dados/lista/ListaEncadeada.java
+++ src/com/maicon/estrutura_dados/lista/ListaEncadeada.java
@@ -1,6 +1,23 @@
 package com.maicon.estrutura_dados.lista;
 
-public class ListaEncadeada {
+public class ListaEncadeada<T> {
 
+    private No<T> inicio;
+    private int tamanho;
+
+    public void adicionar(T elemento) {
+        No<T> celula = new No<T>(elemento);
+        this.inicio = celula;
+        this.tamanho++;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada [inicio=" + inicio + "]";
+    }
     
 }
---
+++ src/com/maicon/estrutura_dados/lista/test/ListaEncadeadaTest.java
@@ -0,0 +1,14 @@
+package com.maicon.estrutura_dados.lista.test;
+
+import com.maicon.estrutura_dados.lista.ListaEncadeada;
+
+public class ListaEncadeadaTest {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adicionar(1);
+
+        System.out.println("Tamanho = " + lista.getTamanho());
+        System.out.println(lista);
+    }
+}
